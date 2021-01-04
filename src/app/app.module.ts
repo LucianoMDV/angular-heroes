72f3b854acb6ab6e5295c09aafc62dcb272813f1
Luciano D'Agata <martinvilla481@gmail.com>
2021-01-04 12:22:36 -0300

    agregue 3 componentes: heroes, detalles y pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { DetallesComponent } from './pages/detalles/detalles.component';
+import { TituloPipe } from './pipes/titulo.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DetallesComponent,
+    TituloPipe
   ],
   imports: [
     BrowserModule,
