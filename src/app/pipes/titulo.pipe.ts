576489747393c29d58b98098717897ef82857457
Luciano D'Agata <martinvilla481@gmail.com>
2021-01-04 14:47:00 -0300

    primer filtro personalizado pipe

--- src/app/pipes/titulo.pipe.ts
+++ src/app/pipes/titulo.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TituloPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    let encabezado = 'Album' + value;
+    return encabezado;
   }
 
 }
