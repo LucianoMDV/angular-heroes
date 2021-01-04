b96e63c6deb43953b424f34e1a70c681429595a6
Luciano D'Agata <martinvilla481@gmail.com>
2021-01-04 12:57:16 -0300

    genere la intereface de heroe

---
+++ src/app/interfaces/heroe.ts
@@ -0,0 +1,22 @@
+export interface Heroe {
+  id: string,
+  name: string,
+  powerstats: {
+    intelligence: string,
+    strength: string,
+    speed: string,
+    durability: string,
+    power: string,
+    combat: string
+  },
+  appearance: {
+    male: string
+  },
+  biography: {
+    firstAppearance: string
+  },
+  images: {
+    md: string,
+    lg: string
+  }
+}
